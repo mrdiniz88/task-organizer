90ef10847815d1350a7688a45a7e43cd857f60f4
mrdiniz88 <mr.diniz8888@gmail.com>
2021-12-12 11:37:13 -0300

    atualização

--- Projects/urls.py
+++ Projects/urls.py
@@ -5,5 +5,7 @@ app_name = 'home'
 
 urlpatterns = [
     path('', views.projects, name='projects'),
+    path('project_detail/<int:id>', views.project_detail, name='project_detail'),
+    path('project_detail/<int:id>/tasks_detail/<str:name>', views.task_detail, name='task_detail')
     # path('detail_tasks', views.detail_tasks, name='detail_tasks')
 ]
--- Projects/views.py
+++ Projects/views.py
@@ -1,3 +1,4 @@
+from django.http.response import HttpResponse
 from django.shortcuts import redirect, render
 from .models import Projects, Tasks
 
@@ -14,3 +15,32 @@ def projects(request):
         'tasks': tasks
     }
     return render(request, 'projects.html', context)
+
+@login_required
+def project_detail(request, id):
+    
+    try:
+        project = Projects.objects.filter(user = request.user).get(id = id)
+        tasks = Tasks.objects.filter(project__id = id).filter(project__user = request.user)
+    except:
+        return redirect('home:projects')
+    
+    context = {
+        'project': project,
+        'tasks': tasks
+    }
+    return render(request, 'project_detail.html', context)
+
+def task_detail(request, id, name):
+    
+    try:
+        project = Projects.objects.filter(user = request.user).get(id = id)
+        tasks = Tasks.objects.filter(project__id = id).filter(project__user = request.user).get(name = name)
+    except:
+        return redirect('home:projects')
+    
+    context = {
+        'project': project,
+        'tasks': tasks
+    }
+    return HttpResponse(f'{tasks.name}')
